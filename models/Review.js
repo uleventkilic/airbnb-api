13c412215da1abf278c12bb638328469e7c1f5e2
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2024-11-28 18:40:07 +0300

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,10 +1,26 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-  listingId: { type: mongoose.Schema.Types.ObjectId, ref: "Listing", required: true },
-  userId: { type: String, required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  comment: { type: String },
+  stayId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Booking",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
