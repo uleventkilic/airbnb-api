1ae8e9bec9de4b551e334c2316e5dbf5ee631612
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2024-11-28 18:39:57 +0300

    Update Listing.js

--- models/Listing.js
+++ models/Listing.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 
 const ListingSchema = new mongoose.Schema({
-  noOfPeople: { type: Number, required: true },
-  country: { type: String, required: true },
-  city: { type: String, required: true },
-  price: { type: Number, required: true },
-  rating: { type: Number, default: 0 },
+  noOfPeople: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  host: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Listing", ListingSchema);
