02d8fb6616ea2c5eaca6510e1e76671b128d0918
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2024-11-28 18:39:44 +0300

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,10 +1,23 @@
 const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema({
-  listingId: { type: mongoose.Schema.Types.ObjectId, ref: "Listing", required: true },
-  userId: { type: String, required: true },
-  dateFrom: { type: Date, required: true },
-  dateTo: { type: Date, required: true },
+  dateFrom: {
+    type: String,
+    required: true,
+  },
+  dateTo: {
+    type: String,
+    required: true,
+  },
+  namesOfPeople: {
+    type: [String], // Array of strings
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Booking", BookingSchema);
