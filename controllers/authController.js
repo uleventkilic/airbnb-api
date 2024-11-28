9301d8af274cffd76e86fa451fbb85fd731124f9
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2024-11-28 18:38:42 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,51 +1,26 @@
-const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-
-exports.registerUser = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Bu e-posta zaten kullanılıyor" });
-    }
-
-    const user = await User.create({ username, email, password });
-
-    res.status(201).json({
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-exports.loginUser = async (req, res) => {
+exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Kullanıcıyı bul
     const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: "Geçersiz email veya şifre" });
     }
 
-    const isMatch = await user.matchPassword(password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Yanlış şifre" });
-    }
+    // JWT Token oluştur
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
 
     res.status(200).json({
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
+      message: "Giriş başarılı",
+      token,
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
