8f73a6707f0fdc5fb6bd3d65f905745ed3ad79c8
uleventkilic <91915538+uleventkilic@users.noreply.github.com>
2024-11-28 18:38:06 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB bağlantısı başarılı");
   } catch (err) {
-    console.error(`Error: ${err.message}`);
+    console.error("MongoDB bağlantı hatası:", err.message);
     process.exit(1);
   }
 };
